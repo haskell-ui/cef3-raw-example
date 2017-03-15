39f72c245befbcc39811c1745fee5c675dfc453e
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-03-15 17:58:45 +0000

    Create window manually [skip travis]

--- src/Main-Windows.hs
+++ src/Main-Windows.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Control.Monad (unless, void)
+import Control.Monad (unless, void, forever)
 import Foreign.Ptr
 import Foreign.Marshal hiding (void)
 
@@ -12,8 +12,8 @@ import Common
 
 main :: IO ()
 main = do
-    hinst <- getModuleHandle Nothing
-    mainArgs <- new $ C'cef_main_args_t hinst
+    mainInstance <- getModuleHandle Nothing
+    mainArgs <- new $ C'cef_main_args_t mainInstance
 
     app <- initialize_app_handler
     exCode <- c'cef_execute_process mainArgs app nullPtr
@@ -23,33 +23,87 @@ main = do
         putStrLn "cef_initialize"
         void $ c'cef_initialize mainArgs settings app nullPtr
 
-        putStrLn "new windowInfo"
-        -- windowInfo <- new $ C'cef_window_info_t nullPtr 0 0 nullPtr
-        winName <- mkCefString "Test window"
-        windowInfo <- new $ C'cef_window_info_t
-            wS_EX_CLIENTEDGE
-            winName
-            wS_OVERLAPPEDWINDOW
-            (fromIntegral cW_USEDEFAULT)
-            (fromIntegral cW_USEDEFAULT)
-            400 300
-            nullPtr -- Parent_Window
-            nullPtr -- Menu
-            False   -- window_rendering_disabled
-            False   -- transparent_painting
-            nullPtr -- Window?
-
-        cefUrl <- mkCefStringPtr "http://www.google.com"
-
-        browserSettings <- mkBrowserSettings
-        client <- initialize_client_handler
-
-        putStrLn "cef_browser_host_create_browser"
-        void $ c'cef_browser_host_create_browser
-            windowInfo client cefUrl browserSettings nullPtr
+        -- register class & create window
+        setupWindow mainInstance 200 200
 
         putStrLn "cef_run_message_loop"
         c'cef_run_message_loop
         putStrLn "cef_shutdown"
         c'cef_shutdown
+
+setupWindow :: HINSTANCE -> Int -> Int -> IO ()
+setupWindow mainInstance width height wndProc = do
+  let winClass = mkClassName "Hello"
+  icon    <- loadIcon   Nothing iDI_APPLICATION
+  cursor  <- loadCursor Nothing iDC_ARROW
+  bgBrush <- createSolidBrush (rgb 0 0 255)
+  registerClass
+      ( cS_VREDRAW + cS_HREDRAW
+      , mainInstance
+      , Just icon
+      , Just cursor
+      , Just bgBrush
+      , Nothing
+      , winClass
+      )
+  lpps <- mallocBytes siceofPAINTSTRUCT
+  hwnd <- createWindow
+              winClass
+              "Test window"
+              wS_OVERLAPPEDWINDOW
+              Nothing Nothing -- initail window pos: default
+              (Just width) (Just height)
+              Nothing         -- no parent, i.e, root window is the parent.
+              Nothing         -- no menu handle
+              mainInstance
+              (wndProc lpps)
+  showWindow hwnd sW_SHOWNORMAL
+  updateWindow hwnd
+
+messagePump :: HWND -> IO ()
+messagePump hwnd = allocaMessage $ \ msg -> forever $ do
+    getMessage msg (Just hwnd) `catch` \ _ -> exitWith ExitSuccess
+    translateMessage msg
+    dispatchMessage msg
+
+wndProc :: LPPAINTSTRUCT
+    -> HWND -> WindowMessage -> WPARAM -> LPARAM -> IO LRESULT
+wndProc lpps hwnd wmsg wParam lParam
+ -- | wmsg == wM_PAINT && hwnd /= nullPtr = do
+     -- r <- getClientRect hwnd
+     -- hdc <- beginPaint hwnd lpps
+     -- endPaint hwnd lpps
+     -- return 0
+ | otherwise =
+     defWindowProc (Just hwnd) wmsg wParam lParam
+ | wmsg == wM_PAINT && hwnd /= nullPtr = do
+    -- (left, top, right, bottom) <- getClientRect hwnd
+    startBrowserWindow hwnd
+    return 0
+
+startBrowserWindow hwnd = do
+    putStrLn "new windowInfo"
+    -- windowInfo <- new $ C'cef_window_info_t nullPtr 0 0 nullPtr
+    winName <- mkCefString "Test window"
+    windowInfo <- new $ C'cef_window_info_t
+        wS_EX_CLIENTEDGE
+        winName
+        -- wS_OVERLAPPEDWINDOW
+        wS_CHILD
+        (fromIntegral cW_USEDEFAULT)
+        (fromIntegral cW_USEDEFAULT)
+        400 300
+        hwnd    -- Parent_Window
+        nullPtr -- Menu
+        False   -- window_rendering_disabled
+        False   -- transparent_painting
+        nullPtr -- Window?
+
+    client <- initialize_client_handler
+    cefUrl <- mkCefStringPtr "http://www.google.com"
+    browserSettings <- mkBrowserSettings
+
+    putStrLn "cef_browser_host_create_browser"
+    void $ c'cef_browser_host_create_browser
+        windowInfo client cefUrl browserSettings nullPtr
 
